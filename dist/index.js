b2991188a8fbb20c98e69b27aaab1177c7c2e51c
pedrovinhas <pstvideos@gmail.com>
2022-12-16 16:56:26 -0300

    deploy: dist again

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const node_path_1 = __importDefault(require("node:path"));
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = require("./app/routes");
@@ -20,7 +19,7 @@ mongoose_1.default.connect(`${process.env.MONGODB_URI}`)
         next();
     });
     app.use(express_1.default.static('public'));
-    app.use('/uploads', express_1.default.static(node_path_1.default.resolve(__dirname, '..', 'uploads')));
+    // app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express_1.default.json());
     app.use(routes_1.router);
     app.listen(PORT, () => {
